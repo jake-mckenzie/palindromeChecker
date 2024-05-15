369254c0c65bb9d0d42fed072775bb1e8c2d55f0
jake-mckenzie <51035453+jake-mckenzie@users.noreply.github.com>
2024-05-15 14:56:04 -0700

    initial commin, added html,css, and js files

---
+++ script.js
@@ -0,0 +1,26 @@
+const textInputElement = document.getElementById('text-input');
+const checkbtnElement = document.getElementById('check-btn');
+const resultElement = document.getElementById('result');
+
+function getResult() {  
+  const inputRaw = textInputElement.value;
+  const input = inputRaw.trim().toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  let inputReverse = "";
+
+  if (input === "") {
+    alert("Please input a value");
+  } else {
+    for (let i = input.length - 1; i >= 0; i--) {
+      inputReverse += input[i];
+    }
+  }
+
+  if (input.length === 1 ||
+      input === inputReverse) {
+    resultElement.innerText = `${inputRaw} is a palindrome`;
+  } else if (input !== inputReverse) {
+    resultElement.innerText = `${inputRaw} is not a palindrome`;
+  } else {
+    resultElement.innerText = ``;
+  }
+}
